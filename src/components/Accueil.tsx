fc14c72929b9ce170a0095e83ae9d9686dad2634
Julien <julienmaure.pro@gmail.com>
2025-01-31 14:34:54 +0100

    add Home part

--- src/components/Accueil.tsx
+++ src/components/Accueil.tsx
@@ -1,13 +1,32 @@
+import Image from "next/image";
+import portrait from "../assets/portrait.jpg";
+
 const Acceuil = () => {
   return (
-    <div className="flex flex-col-reverse md:flex-row justify-center items-center md:my-32 my-10">
+    <div className="flex flex-col-reverse md:flex-row justify-center items-center md:my-32 my-10 mx-5">
       <div>
-        <h1 className="text-3xl md:text-5xl font-bold text-center md:text-left">
+        <h1 className="text-3xl md:text-5xl font-bold text-center md:text-left mt-4 md:mt-0">
           Bonjour, <br /> je suis{" "}
           <span className="text-accent">Julien Mauré</span>
         </h1>
+        <h2 className="text-xl md:text-2xl text-center md:text-left mt-4">
+          Développeur Full Stack
+        </h2>
+        <p className="my-4 text-md text-center md:text-left md:max-w-lg">
+          Diplômé d’une licence APSIO avec un an d’expérience dans une ESN, j’ai
+          par la suite développé de nouvelles compétences dans un autre secteur
+          d’activé. Avec pour projet de trouver un poste dans le développement
+          informatique, j’ai terminé une POE pour mettre à jour mes
+          connaissances et en acquérir de nouvelles.{" "}
+        </p>
       </div>
-      <div>Ici c est la photo</div>
+      <div className="md:ml-32">
+        <Image
+          src={portrait}
+          alt="portrait image"
+          className="w-80 h-80 object-cover border-8 border-accent shadow-xl rounded-full"
+        />
+      </div>
     </div>
   );
 };
